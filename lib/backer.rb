833af50e96457991789ff790e4d74edd833d7735
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-09 17:01:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,10 @@
 class Backer
   attr_reader :backed_projects
-  @backed_projects = []
+  
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
   
   def back_project()
   end
